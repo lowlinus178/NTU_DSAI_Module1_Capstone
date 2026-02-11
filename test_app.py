df5142ab4c0d59757a03d2f822f04c610b37161a
lowlinus178 <lowlinus178@gmail.com>
2026-02-11 14:21:08 +0800

    Delete test_app.py

--- test_app.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Minimal Test App", layout="centered")
-
-st.title("Hello from Render!")
-st.write("If you can see this without a 502, your deployment is fine.")
-st.button("Click me")
